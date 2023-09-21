43501fc9f2977eb70efbb907b0de1b57a45a6b68
je <qwer90218@gmail.com>
2023-09-21 15:24:59 +0800

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,20 @@ export default {
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
-    './app.vue'
+    './app.vue',
   ],
   theme: {
     extend: {
       colors: {
-        text: {
-          10: 'hsl(var(--color-text10) / <alpha-value>)',
-          50: 'hsl(var(--color-text50) / <alpha-value>)',
-          100: 'hsl(var(--color-text100) / <alpha-value>)'
-        },
-        bkg: 'hsl(var(--color-bkg) / <alpha-value>)'
+        // text: {
+        //   10: 'hsl(var(--color-text10) / <alpha-value>)',
+        //   50: 'hsl(var(--color-text50) / <alpha-value>)',
+        //   100: 'hsl(var(--color-text100) / <alpha-value>)',
+        // },
+        bkg: 'hsl(var(--color-bkg) / <alpha-value>)',
         // text100: 'hsl(var(--color-text100) / <alpha-value>)'
-      }
-    }
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
