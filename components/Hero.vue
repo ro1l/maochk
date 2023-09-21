dc3a0d4ceff29043c4d90332ee82c7d2cd8c8ddc
je <qwer90218@gmail.com>
2023-09-21 15:16:08 +0800

    test

--- components/Hero.vue
+++ components/Hero.vue
@@ -6,7 +6,7 @@
     <!-- Gradients -->
     <div aria-hidden="true" class="flex absolute -top-96 left-1/2 transform -translate-x-1/2">
       <div class="bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem] dark:from-violet-900/50 dark:to-purple-900" />
-      <div class="bg-gradient-to-tl from-blue-100 via-blue-100 to-blue-50 blur-3xl w-[90rem] h-[50rem] origin-top-left -rotate-12 -translate-x-[15rem] dark:from-indigo-900/70 dark:via-indigo-900/70 dark:to-blue-900/70 rounded-fulls" />
+      <div class="bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl w-[90rem] h-[50rem] rounded-fulls origin-top-left -rotate-12 -translate-x-[15rem] dark:from-indigo-900/70 dark:via-indigo-900/70 dark:to-blue-900/70" />
     </div>
     <!-- End Gradients -->
 
