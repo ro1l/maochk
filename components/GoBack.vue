2a439595c527e31dcf28197a10a2708c9addae15
je <qwer90218@gmail.com>
2023-09-26 18:15:11 +0800

    clear todo

--- components/GoBack.vue
+++ components/GoBack.vue
@@ -1,12 +1,22 @@
 <script setup>
+const store = useTodosStore();
 const router = useRouter();
 
 function goBack() {
+  store.todo = [];
   return router.go(-1);
 }
 </script>
 <template>
-  <button @click="goBack">
-    回上一頁
-  </button>
+  <div class="flex items-center pb-5">
+    <div class="text-black me-2">
+      <IconCaretLeft />
+    </div>
+    <button
+      class=""
+      @click="goBack"
+    >
+      返回
+    </button>
+  </div>
 </template>
